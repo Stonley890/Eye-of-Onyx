2abaf7c134b16ffbb542402128fa51a12c694fe9
Stonley890 <79172597+Stonley890@users.noreply.github.com>
2023-03-22 08:05:33 -0700

    More framework

--- eyeofonyx/src/main/java/io/github/stonley890/Main.java
+++ eyeofonyx/src/main/java/io/github/stonley890/Main.java
@@ -2,10 +2,21 @@ package io.github.stonley890;
 
 import org.bukkit.Bukkit;
 
-public class Main 
-{
-    public void onEnable()
-    {
-        Bukkit.getLogger().info( "Hello World!" );
+public class Main {
+
+    public static Main plugin;
+
+    public void onEnable() {
+        // Register variables
+        plugin = this;
+
+        String version = getDescription().getVersion();
+
+        // Create config if needed
+        getDataFolder().mkdir();
+        saveDefaultConfig();
+
+        // Start message
+        Bukkit.getLogger().info("Eye of Onyx " + version + ": A plugin that manages the royalty board on Wings of Fire: The New World");
     }
 }
