d2f9f037d43c44e7660a8034add45c96389c20bd
Stonley890 <79172597+Stonley890@users.noreply.github.com>
2023-05-01 17:56:00 -0700

    Make a functioning plugin
    - Modified pom.xml and plugin.yml for successful build and run
    - Added `eyeofonyx` command for version number
    - Added writeConfig method for easy config writing

--- eyeofonyx/src/main/java/io/github/stonley890/Main.java
+++ eyeofonyx/src/main/java/io/github/stonley890/Main.java
@@ -1,22 +1,46 @@
 package io.github.stonley890;
 
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main {
+import io.github.stonley890.commands.CmdEyeOfOnyx;
+
+/*
+ * The main ticking thread.
+*/
+
+public class Main extends JavaPlugin {
 
     public static Main plugin;
+
+    public static String version;
 
     public void onEnable() {
         // Register variables
         plugin = this;
 
-        String version = getDescription().getVersion();
-
+        version = getDescription().getVersion();
+        
         // Create config if needed
         getDataFolder().mkdir();
         saveDefaultConfig();
 
+        // Initialize command executors
+        getCommand("eyeofonyx").setExecutor(new CmdEyeOfOnyx());
+
         // Start message
         Bukkit.getLogger().info("Eye of Onyx " + version + ": A plugin that manages the royalty board on Wings of Fire: The New World");
+    }
+
+    public boolean writeConfig(String path, Object value) {
+        try {
+            getConfig().set(path, value);
+            saveConfig();
+            return true;
+        } catch (Exception e) {
+            getLogger().severe("Error writing to config.yml:\n" + e);
+            return false;
+        }
+        
     }
 }
---
+++ eyeofonyx/src/main/java/io/github/stonley890/commands/CmdEyeOfOnyx.java
@@ -0,0 +1,17 @@
+package io.github.stonley890.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+import io.github.stonley890.Main;
+import net.md_5.bungee.api.ChatColor;
+
+public class CmdEyeOfOnyx implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        sender.sendMessage(ChatColor.YELLOW + "Eye of Onyx " + Main.version + "\nStonley890 / iHeron");
+        return true;
+    }
+}
